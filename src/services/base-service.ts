2ee5d8fd324478197dd84e2e59b3496aa98c605f
Original-pokemon <eremkin-02@mail.ru>
2024-01-23 17:45:33 +0300

    feat: error handle for api

--- src/services/base-service.ts
+++ src/services/base-service.ts
@@ -10,29 +10,48 @@ export class BaseService<T> extends ApiService {
   }
 
   getAll = async (): Promise<T[]> => {
-    const { data } = await this.api.get<T[]>(this.resource.All);
-    return data;
+    try {
+      const { data } = await this.api.get<T[]>(this.resource.All);
+      return data;
+    } catch {
+      throw new Error("Error getting all items");
+    }
   };
 
   getSelect = async (ids: string[]): Promise<T[]> => {
     if (this.resource.Many) {
-      const { data } = await this.api.post<T[]>(this.resource.Many, {
-        data: ids,
-      });
-      return data;
+      try {
+        const { data } = await this.api.post<T[]>(this.resource.Many, {
+          data: ids,
+        });
+        return data;
+      } catch {
+        throw new Error("Error getting many items");
+      }
     }
     throw new Error(`Method don't implement`);
   };
 
   getUnique = async (id: string): Promise<T> => {
-    const { data } = await this.api.get<T>(this.resource.Info(id));
-    return data;
+    try {
+      const { data } = await this.api.get<T>(this.resource.Info(id));
+      return data;
+    } catch {
+      throw new Error("Error getting item");
+    }
   };
 
   create = async (item: T): Promise<string> => {
     if (this.resource.Create) {
-      const { data } = await this.api.post<string>(this.resource.Create, item);
-      return data;
+      try {
+        const { data } = await this.api.post<string>(
+          this.resource.Create,
+          item,
+        );
+        return data;
+      } catch {
+        throw new Error("Error creating item");
+      }
     }
     throw new Error("this method is not implemented");
   };
@@ -43,11 +62,15 @@ export class BaseService<T> extends ApiService {
       const itemId = id || userId;
 
       if (itemId) {
-        const { data } = await this.api.put<T>(
-          this.resource.Update(itemId),
-          item,
-        );
-        return data;
+        try {
+          const { data } = await this.api.put<T>(
+            this.resource.Update(itemId),
+            item,
+          );
+          return data;
+        } catch {
+          throw new Error("Error updating item");
+        }
       }
       throw new Error("Missing 'id' property in the item.");
     }
@@ -56,8 +79,12 @@ export class BaseService<T> extends ApiService {
 
   delete = async (id: string): Promise<T> => {
     if (this.resource.Delete) {
-      const { data } = await this.api.delete<T>(this.resource.Delete(id));
-      return data;
+      try {
+        const { data } = await this.api.delete<T>(this.resource.Delete(id));
+        return data;
+      } catch {
+        throw new Error("Error deleting item");
+      }
     }
     throw new Error("this method is not implemented");
   };
