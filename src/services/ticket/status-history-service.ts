5af1e329f0bbc1207515d42c60f6a806bcf80cd9
Original-pokemon <eremkin-02@mail.ru>
2024-02-26 17:37:49 +0300

    refactor: update status history type

--- src/services/ticket/status-history-service.ts
+++ src/services/ticket/status-history-service.ts
@@ -2,10 +2,11 @@ import { BaseService } from "../base-service.js";
 import { ApiRouteKey } from "../const.js";
 
 type StatusHistoryType = {
+  id: string;
   ticket_id: string;
   user_id: string;
   ticket_status: number;
-  updated_at: Date;
+  created_at: Date;
 };
 
 export class StatusHistoryService extends BaseService<StatusHistoryType> {
