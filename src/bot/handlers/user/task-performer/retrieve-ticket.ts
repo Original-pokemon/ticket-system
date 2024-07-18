eff11aa7388a30ebdcf10b04c9e5c58fb8c7113e
original-pokemon <eremkin-02@mail.ru>
2024-07-18 13:10:26 +0300

    refactor:  use new func

--- src/bot/handlers/user/task-performer/retrieve-ticket.ts
+++ src/bot/handlers/user/task-performer/retrieve-ticket.ts
@@ -3,41 +3,25 @@ import { CallbackQueryContext } from "grammy";
 import { TicketStatus, UserText } from "#root/bot/const/index.js";
 import { TicketType } from "#root/services/index.js";
 import { retrieveTicketData } from "#root/bot/callback-data/index.js";
+import { sendManagers } from "#root/bot/helpers/index.js";
 
 type Properties = {
   ctx: Context;
   ticket: TicketType;
 };
 
-const sendManagers = async ({ ctx, ticket }: Properties, text: string) => {
-  const { petrol_station_id: petrolStationId } = ticket;
-  const { managers } =
-    await ctx.services.PetrolStation.getUnique(petrolStationId);
-
-  if (!managers) {
-    throw new Error("Managers not found");
-  }
-
-  const promises = managers.map(async (managerId) => {
-    await ctx.api.sendMessage(managerId, text);
-  });
-
-  Promise.all(promises);
-};
-
 const sendManagersNotificationAboutRetrieveTicket = async ({
   ctx,
   ticket,
 }: Properties) => {
-  await ctx.services.Ticket.update({
-    ...ticket,
-    status_id: TicketStatus.ReviewedManager,
-    status_history: [
-      {
-        user_id: ctx.session.user.id,
-        ticket_status: TicketStatus.ReviewedManager,
-      },
-    ],
+  if (!ticket.id) {
+    throw new Error("Ticket Id not found");
+  }
+
+  await ctx.services.Ticket.updateTicketStatus({
+    statusId: TicketStatus.ReviewedManager,
+    ticketId: ticket.id,
+    userId: ctx.session.user.id,
   });
 
   await sendManagers(
